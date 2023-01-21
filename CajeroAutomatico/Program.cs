be2956a1a49c9187178e8a6928594dd719cccbd5
Alin Piero <alinpiero@gmail.com>
2023-01-21 15:34:01 -0500

    Sistema mejorado en C#

--- CajeroAutomatico/Program.cs
+++ CajeroAutomatico/Program.cs
@@ -10,6 +10,200 @@ namespace CajeroAutomatico
     {
         static void Main(string[] args)
         {
+            String numeroTarjeta="",cuentaDestino, cuentaOrigen;
+            int clave=0,contador=1, op=0, opcionRetiro;
+            double saldoTotal=1000, montoTransferir, montoRetirar, otroMontoRetiro,montoOrigen;
+
+            while (numeroTarjeta != "123456789" || clave!=1234)
+            {
+                Console.Clear();
+                Console.WriteLine("Ingresar su numero de Tarjeta: ");
+                numeroTarjeta = Console.ReadLine();
+
+                Console.WriteLine("Ingresar su clave: ");
+                clave = Convert.ToInt32(Console.ReadLine());
+
+                if(numeroTarjeta!="1234567890" || clave != 123)
+                {
+                   
+                    
+                    
+                    if (contador == 3)
+                    {
+                        Console.WriteLine("Lo sentimos excedió número de veces :( ");
+                        Console.ReadLine();
+                        goto final;
+                    }
+                    contador++;
+                }
+                else
+                {
+                    Console.WriteLine("Bienvenido a su cuenta bancaria\n");
+                    do
+                    {
+                        operaciones:
+                        Console.WriteLine("OPERACIONES A REALIZAR ");
+                        Console.WriteLine("******************************************\n");
+                        Console.WriteLine("1: Consultar Saldo ");
+                        Console.WriteLine("2: Transferencia a otra Cuenta ");
+                        Console.WriteLine("3: Retirar Dinero ");
+                        Console.WriteLine("4: Recargar Saldo ");
+                        Console.WriteLine("5: Salir\n ");
+                        Console.WriteLine("\n******************************************\n");
+
+                        Console.WriteLine("Qué operación desea realizar? ");
+                        op = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("\n");
+
+                        switch (op)
+                        {
+                            case 1:
+                                Console.WriteLine("***Consulta de Saldo***");
+                                Console.WriteLine("Su Saldo Total es: "+saldoTotal +" Soles\n");
+                            break; goto operaciones;
+
+                            case 2:
+                                Console.WriteLine("***Transferencia a otra Cuenta***\n");
+                                Console.WriteLine("Ingresar cuenta de destino: ");
+                                cuentaDestino = Console.ReadLine();
+                                Console.WriteLine("Ingresar el Monto a Transferir: ");
+                                montoTransferir = double.Parse(Console.ReadLine());
+                                
+                                if (montoTransferir >= saldoTotal)
+                                {
+                                    Console.WriteLine("Saldo Total: " + saldoTotal);
+                                    Console.WriteLine("Monto a transferir es: " + montoTransferir);
+                                    Console.WriteLine("El saldo Total es menor que el monto a Destinar");
+                                    Console.WriteLine("Ingresar monto menor al saldo Total\n ");
+                                    goto operaciones;
+                                }
+                                else
+                                {
+                                    saldoTotal = saldoTotal - montoTransferir;
+                                    Console.WriteLine("Saldo Total: " + saldoTotal);
+                                    Console.WriteLine("Monto transferido es: "+montoTransferir);
+                                    Console.WriteLine("¡Transferencia Exitosa!");                                 
+                                }
+                            break; goto operaciones;
+
+                            case 3:
+                                Console.WriteLine("***Retiro de Dinero***\n");
+                                Console.WriteLine("===Opciones de Retiro===");
+                                Console.WriteLine("1:s/.20");
+                                Console.WriteLine("2:s/:50");
+                                Console.WriteLine("3:s/.100");
+                                Console.WriteLine("4:s/:200");
+                                Console.WriteLine("5:s/ otro monto");
+                                
+                                Console.WriteLine("Ingresar la opción de Retiro:");
+                                opcionRetiro =int.Parse(Console.ReadLine());                            
+                                switch (opcionRetiro)
+                                {
+                                    case 1:
+                                        if (20 >= saldoTotal)
+                                        {
+                                            Console.WriteLine("Saldo insuficiente");
+                                            Console.WriteLine("Ingresar monto menor o igual al saldo Total\n ");
+                                            goto operaciones;
+                                        }
+                                        else
+                                        {
+                                            saldoTotal = saldoTotal - 20;
+                                            Console.WriteLine("Saldo Total: " + saldoTotal);
+                                            Console.WriteLine("Monto retirado es: " + "s/.20");                                        
+                                            Console.WriteLine("¡Ha retirado satisfactoriamente!");                                         
+                                        }
+                                        break; goto operaciones;                                       
+                                    case 2:
+                                        if (50 >= saldoTotal)
+                                        {
+                                            Console.WriteLine("Saldo insuficiente");
+                                            Console.WriteLine("Ingresar monto menor o igual al saldo Total\n ");
+                                            goto operaciones;
+                                        }
+                                        else
+                                        {
+                                            saldoTotal = saldoTotal - 50;
+                                            Console.WriteLine("Saldo Total: " + saldoTotal);
+                                            Console.WriteLine("Monto retirado es: " + "s/.50");
+                                            Console.WriteLine("¡Ha retirado satisfactoriamente!");
+                                        }
+                                        break; goto operaciones;
+                                    case 3:
+                                        if (100 >= saldoTotal)
+                                        {
+                                            Console.WriteLine("Saldo insuficiente");
+                                            Console.WriteLine("Ingresar monto menor o igual al saldo Total\n ");
+                                            goto operaciones;
+                                        }
+                                        else
+                                        {
+                                            saldoTotal = saldoTotal - 100;
+                                            Console.WriteLine("Saldo Total: " + saldoTotal);
+                                            Console.WriteLine("Monto retirado es: " + "s/.100");
+                                            Console.WriteLine("¡Ha retirado satisfactoriamente!");
+                                        }
+                                        break; goto operaciones;
+                                    case 4:
+                                        if (200 >= saldoTotal)
+                                        {
+                                            Console.WriteLine("Saldo insuficiente");
+                                            Console.WriteLine("Ingresar monto menor o igual al saldo Total\n ");
+                                            goto operaciones;
+                                        }
+                                        else
+                                        {
+                                            saldoTotal = saldoTotal - 200;
+                                            Console.WriteLine("Saldo Total: " + saldoTotal);
+                                            Console.WriteLine("Monto retirado es: " + "s/.200");
+                                            Console.WriteLine("¡Ha retirado satisfactoriamente!");
+                                        }
+                                        break; goto operaciones;
+                                    case 5:
+                                        Console.WriteLine("Ingrese otro monto de retiro");
+                                        otroMontoRetiro = double.Parse(Console.ReadLine());
+                                        saldoTotal = saldoTotal - otroMontoRetiro;
+                                        Console.WriteLine("Monto Retirado " + "s/. "+otroMontoRetiro);
+                                        Console.WriteLine("¡Ha retirado satisfactoriamente!");
+                                        break; goto operaciones;
+                                   default: 
+                                        Console.WriteLine("Opcion incorrecta");
+                                        break; goto operaciones;
+                                }
+                            break;
+                            case 4:
+                                Console.WriteLine("***Recargar  Saldo***\n");
+                                Console.WriteLine("==============");
+                                Console.WriteLine("Ingresar la cuenta de Origen ");
+                                cuentaOrigen = Console.ReadLine();
+                                Console.WriteLine("Ingresar monto de Origen ");
+                                montoOrigen = double.Parse(Console.ReadLine());
+
+                                saldoTotal=saldoTotal + montoOrigen;
+                                break;goto operaciones;
+
+                            default:Console.WriteLine("Salir\n");
+                            break;
+                                goto operaciones;
+                        }
+                    }
+                    while (op != 5);
+                    goto final;
+                    //Console.ReadKey();
+                }
+
+            }
+            final:
+            Console.WriteLine("\n******************************************");
+            Console.WriteLine("Retire su tarjeta.");
+            Console.Write("Gracias por su visita. Hasta pronto.....");
+            Console.WriteLine("\n******************************************");
+           
+            Console.ReadKey();
+
+
+
+            /*
             string tarj = "", pass = "", numc = "";
             int cont = 0, op = 0;
             double saldo = 10000, mon = 0, a, dep = 0, desc = 0;
@@ -196,17 +390,18 @@ namespace CajeroAutomatico
                 }
             } while (op != 4);
         fin:
-        /*Console.Clear();
+        Console.Clear();
         Console.WriteLine("\n--------------------------------------");
         Console.WriteLine("\n\n** Su saldo es S/ {0} **", saldo);
         Console.WriteLine("Se le descontó por sus operaciones : S/{0}",des
-       c);*/
+       c);
         final:
             Console.WriteLine("\n******************************************"
            );
             Console.WriteLine("Retire su tarjeta.");
             Console.Write("Gracias por su visita. Hasta pronto.....");
             Console.ReadLine();
+            */
         }
     }
 }
